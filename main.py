a9859713c0b76c0c56cf2b4846a06ee826af18a8
Huan Sun <hs3240@columbia.edu>
2023-01-30 16:48:44 -0500

    add front end files

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ app = create_app()
 if __name__ == '__main__': 
     # allows to execute the code when when the file runs as a script, but not when it's imported as a module 
     app.run(debug= True)
+
--- website/views.py
+++ website/views.py
@@ -1,10 +1,10 @@
-from flask import Blueprint # a way to seperate my web app out
+from flask import Blueprint, render_template # a way to seperate my web app out
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     # this function will run whenever go to the '/' root
-    return "<h1>Test</h1>"
+    return render_template("home.html")
     
 
