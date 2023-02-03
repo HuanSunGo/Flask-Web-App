b373c8abfa8d95df9cd3475e771941d11e846674
Huan Sun <hs3240@columbia.edu>
2023-02-02 23:26:36 -0500

    use login manager

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager # manages what web pages that we can access and what we cannot 
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -10,6 +11,10 @@ def create_app():
     app.config['SECRET_KEY'] = ['hiangfaihueagurntagtngxa']
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
     
     from .views import views
     from .auth import auth 
@@ -20,9 +25,12 @@ def create_app():
 
     # need to import the .models to make sure to define the classes before initialize the database
     from .models import User, Note
-    return app
 
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app = app)
-        print('Created Database!')
+    with app.app_context():
+        db.create_all()  
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id)) # works similar as filter by in query  
+    
+    return app
--- website/views.py
+++ website/views.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template # a way to seperate my web app out
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     # this function will run whenever go to the '/' root
-    return render_template("home.html")
+    return render_template("home.html", user = current_user)
     
 
